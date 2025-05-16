e25b911aea5e6ffced4962af217d3e4d44c4a592
Documenter.jl <documenter@juliadocs.github.io>
2025-05-16 13:13:54 +0000

    build based on 621213f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MultibodyDynamicsLite","page":"Home","title":"MultibodyDynamicsLite","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MultibodyDynamicsLite.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
