a43641becd2467d6e5f749ec8b75af8932b7c053
Grzegorz Orzechowski <2590699+gorzech@users.noreply.github.com>
2025-05-16 14:57:53 +0200

    chore: initial skeleton via PkgTemplates

--- src/MultibodyDynamicsLite.jl
+++ src/MultibodyDynamicsLite.jl
@@ -1,5 +1,23 @@
 module MultibodyDynamicsLite
 
-# Write your package code here.
+export Body6D, RevoluteJoint, assemble, step_euler!
+
+struct Body6D
+    r::SVector{3,Float64}   # position
+    q::SVector{4,Float64}   # unit quaternion
+    m::Float64
+    Ic::SMatrix{3,3,Float64}
+end
+
+struct RevoluteJoint
+    body_i::Int
+    body_j::Int
+    s_i::SVector{3,Float64}
+    s_j::SVector{3,Float64}
+end
+
+# placeholder functions -------------------------------------------------
+step!(::AbstractVector, ::AbstractVector, ::Real) = error("todo")
+assemble(::Vector{Body6D}, ::Vector{RevoluteJoint}) = error("todo")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,9 @@ using Test
 
 @testset "MultibodyDynamicsLite.jl" begin
     # Write your tests here.
+
+    @testset "step!" begin
+        # dummy unit test to keep CI green
+        @test true
+    end
 end
