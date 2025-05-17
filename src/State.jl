9d5a42fd24cb57e214c1c4a5f510584617913010
lukrow80 <64228214+lukrow80@users.noreply.github.com>
2025-05-17 16:35:32 +0200

    Added get_index, init_state and prototypes
    * feature: Abstract Constraint types
    
    * Update State.jl, AbstractConstraintsTests.jl, StateTests.jl, and 1 more file
    
    * Update MultibodyDynamicsLite.jl and runtests.jl
    
    * Completed init_state() function in state
    
    * initial version
    
    * draft version
    
    * Added get_index function
    
    * Updated return type in get_index to int
    
    * Fixed get_index
    
    ---------
    
    Co-authored-by: BoredPlayer <oskar.g.rpi@vp.pl>
    Co-authored-by: BoredPlayer <51460116+BoredPlayer@users.noreply.github.com>

--- src/State.jl
+++ src/State.jl
@@ -1,4 +1,4 @@
-export State
+export State, init_state, get_index
 
 struct State
     time::Float64
@@ -7,4 +7,34 @@ struct State
     function State(;time = 0, q = zeros(7))
         new(time, q)
     end
+end
+
+function init_state(bodies::Vector{Body})
+    # Detect sizes of positional and rotational vectors
+    pos_num = length(bodies[1].r)
+    rot_num = length(bodies[1].e)
+    # Caluclate the size of a body's state vector
+    ove_num = pos_num + rot_num
+    temp_q = zeros(ove_num*length(bodies))
+    for (i, body) in enumerate(bodies)
+        for (j, r) in enumerate(body.r)
+            temp_q[(i-1)*ove_num+j] = r
+        end
+        for(j, e) in enumerate(body.e)
+            temp_q[(i-1)*ove_num+pos_num+j] = e
+        end
+    end
+    return State(time = 0, q = temp_q)
+end
+
+function get_index(body_pile::Vector{Body}, body::Body)
+    # Detect sizes of positional and rotational vectors
+    pos_num = length(body_pile[1].r)
+    rot_num = length(body_pile[1].e)
+    # Caluclate the size of a body's state vector
+    ove_num = pos_num + rot_num
+    # Find all matching objects
+    list_of_matching_bodies = findfirst(x -> x == body, body_pile)
+    # Return the first one (should be the only one)
+    return list_of_matching_bodies
 end
