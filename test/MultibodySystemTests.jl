6f916b436cfdd274733a4a65e7269593baa67478
paczk <paczka1234567890@gmail.com>
2025-05-17 13:40:27 +0200

    test: added tests for constructor with name

--- test/MultibodySystemTests.jl
+++ test/MultibodySystemTests.jl
@@ -3,4 +3,9 @@ using Test
 
 @testset "MultibodySystemTests.jl" begin
     @test_nowarn multibodysystem_def = MultibodySystem()
+    system = MultibodySystem("test")
+    @test system.name == "test"
+    @test system.bodies == []
+    @test system.kinematic_contstraints == []
+    @test system.driving_constraints == []
 end
