c8d14eac0ab5d622f26ee9a422b9ae1e8ebb6a1c
paczk <paczka1234567890@gmail.com>
2025-05-17 12:12:23 +0200

    feature: added test for default values

--- test/StateTests.jl
+++ test/StateTests.jl
@@ -3,4 +3,5 @@ using MultibodyDynamicsLite
 
 @testset "State.jl" begin
     @test_nowarn state_test = State(time = 1.0, q = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+    @test_nowarn state_def = State()
 end
