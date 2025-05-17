071f6c293c44f04ad02857c351404754dd7f4cbc
BoredPlayer <oskar.g.rpi@vp.pl>
2025-05-17 16:06:16 +0200

    Added get_index function

--- src/State.jl
+++ src/State.jl
@@ -1,4 +1,4 @@
-export State, init
+export State, init_state
 
 struct State
     time::Float64
@@ -9,7 +9,7 @@ struct State
     end
 end
 
-function init(bodies::Vector{Body})
+function init_state(bodies::Vector{Body})
     # Detect sizes of positional and rotational vectors
     pos_num = length(bodies[1].r)
     rot_num = length(bodies[1].e)
@@ -25,4 +25,16 @@ function init(bodies::Vector{Body})
         end
     end
     return State(time = 0, q = temp_q)
+end
+
+function get_index(body_pile::Vector{Body}, body::Body)
+    # Detect sizes of positional and rotational vectors
+    pos_num = length(bodies[1].r)
+    rot_num = length(bodies[1].e)
+    # Caluclate the size of a body's state vector
+    ove_num = pos_num + rot_num
+    # Find all matching objects
+    list_of_matching_bodies = find(body, body_pile)
+    # Return the first one (should be the only one)
+    return (list_of_matching_bodies[1]-1)*ove_num:(list_of_matching_bodies[1]-1)*ove_num+7
 end
--- test/StateTests.jl
+++ test/StateTests.jl
@@ -8,12 +8,12 @@ end
 
 @testset "Initialising state vector" begin
     bodies = [Body([0.0, 1.0, 2.0], [0.71, 0.0, 0.0, 0.71])]
-    state = init(bodies)
-    @test init(bodies).q[1] == 0.0
-    @test init(bodies).q[2] == 1.0
-    @test init(bodies).q[3] == 2.0
-    @test init(bodies).q[4] == 0.7071067811865476
-    @test init(bodies).q[5] == 0.0
-    @test init(bodies).q[6] == 0.0
-    @test init(bodies).q[7] == 0.7071067811865476
+    state = init_state(bodies)
+    @test init_state(bodies).q[1] == 0.0
+    @test init_state(bodies).q[2] == 1.0
+    @test init_state(bodies).q[3] == 2.0
+    @test init_state(bodies).q[4] == 0.7071067811865476
+    @test init_state(bodies).q[5] == 0.0
+    @test init_state(bodies).q[6] == 0.0
+    @test init_state(bodies).q[7] == 0.7071067811865476
 end
