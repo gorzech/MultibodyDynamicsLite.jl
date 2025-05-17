06d999c26cea91d5c60a86601248433bf56cd94d
lukasz <lukrow@o2.pl>
2025-05-17 12:02:44 +0200

    tests in multibody

--- test/body.jl
+++ test/body.jl
@@ -1,12 +1,6 @@
-using MultibodyDynamicsLite
-using Test
-using LinearAlgebra
 
-
-@testset "Body.jl" begin
-e1 = [1,2,3,4]/norm([1,2,3,4])
     @testset "Body constructor" begin
+        e1 = [1,2,3,4]/norm([1,2,3,4])
         @test_nowarn b1 = Body([1.0, 2.0, 3.0], e1)
         @test_nowarn b2 = Body()
     end
-end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,8 @@
 using MultibodyDynamicsLite
 using Test
+using LinearAlgebra
 
 @testset "MultibodyDynamicsLite.jl" begin
-
-    @testset "Body constructor" begin
-        @test_nowarn b1 = Body([1.0, 2.0, 3.0], [0.0, 0.0, 0.0, 1.0])
-    end
+include("body.jl")
 end
 
-include("body.jl")
