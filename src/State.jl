211f6e861a5ff2d4299199aa860f15368a37ed85
BoredPlayer <oskar.g.rpi@vp.pl>
2025-05-17 13:30:13 +0200

    Update State.jl, AbstractConstraintsTests.jl, StateTests.jl, and 1 more file

--- src/State.jl
+++ src/State.jl
@@ -1,4 +1,4 @@
-export State
+export State, init
 
 struct State
     time::Float64
@@ -7,4 +7,22 @@ struct State
     function State(;time = 0, q = zeros(7))
         new(time, q)
     end
+end
+
+function init(bodies::Vector{Body})
+    # Detect sizes of positional and rotational vectors
+    pos_num = size(bodies[1].r)
+    rot_num = size(bodies[1].e)
+    # Caluclate the size of a body's state vector
+    ove_num = pos_num + rot_num
+    temp_q = Vector(ove_num*size(bodies))
+    for (i, body) in enumerate(bodies)
+        for (j, r) in body.r
+            temp_q[i*ove_num+j] = r[j]
+        end
+        for(j, e) in enumerate(body.e)
+            temp_q[i*ove_num+pos_num+j] = e[j]
+        end
+    end
+    return State(time = 0, q = temp_q)
 end
--- test/StateTests.jl
+++ test/StateTests.jl
@@ -5,3 +5,12 @@ using MultibodyDynamicsLite
     @test_nowarn state_test = State(time = 1.0, q = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
     @test_nowarn state_def = State()
 end
+
+@testset "Initialising state vector" begin
+    bodies = [Body([0.0, 1.0, 2.0], [0.71, 0.0, 0.0, 0.71])]
+    @test_nowarn state = init(bodies)
+    @test init(bodies).q[1] = 0.0
+    @test init(bodies).q[2] = 1.0
+    @test init(bodies).q[3] = 2.0
+    @test init(bodies).q[4] = 1.0
+end
