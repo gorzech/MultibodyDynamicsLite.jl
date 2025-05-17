2ec561c592388e2c6bef3fb1bd63263bef166890
lukasz <lukrow@o2.pl>
2025-05-17 13:22:54 +0200

    final commit

--- test/BodyTests.jl
+++ test/BodyTests.jl
@@ -1,5 +1,21 @@
+using MultibodyDynamicsLite
+using Test
 
-    @testset "Body constructor" begin
-        @test_nowarn b1 = Body([1.0, 2.0, 3.0], [1.0, 0.0, 0.0, 0.0])
-        @test_nowarn b2 = Body()
+@testset "Body constructor default" begin
+    @test_nowarn begin
+        b1 = Body()
+        @test b1.r == [0.0, 0.0, 0.0]
+        @test b1.e == [1.0, 0.0, 0.0, 0.0]
     end
+end
+
+@testset "Body constructor normalization" begin
+    @test_throws ArgumentError Body([1.0, 2.0, 3.0], [0.0, 0.0, 0.0, 0.0])
+
+    @test_nowarn begin
+        b3 = Body([1.0, 2.0, 3.0], [2.0, 0.0, 0.0, 0.0])
+        @test b3.r == [1.0, 2.0, 3.0]
+        @test b3.e == [1.0, 0.0, 0.0, 0.0]
+    end
+end
+
