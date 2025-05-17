07849272003150fb31226de6903550d31e4e2960
BoredPlayer <oskar.g.rpi@vp.pl>
2025-05-17 13:34:13 +0200

    Update MultibodyDynamicsLite.jl and runtests.jl

--- src/MultibodyDynamicsLite.jl
+++ src/MultibodyDynamicsLite.jl
@@ -5,16 +5,4 @@ include("AbstractConstraints.jl")
 include("Body.jl")
 include("State.jl")
 
-<<<<<<< HEAD
-export Body
-
-include("AbstractConstraints.jl")
-
-struct Body
-    r::Vector{Float64}   # position x y z
-    e::Vector{Float64}   # unit quaternion e0 ex ey ez
-end
-
-=======
->>>>>>> main
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,6 @@ using MultibodyDynamicsLite
 using Test
 using LinearAlgebra
 
-<<<<<<< HEAD
-include("test_AbstractConstraints.jl")
-=======
 include("AbstractConstraintsTests.jl")
 include("BodyTests.jl")
->>>>>>> main
 include("StateTests.jl")
