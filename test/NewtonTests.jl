d2f5a6aae79bc3b427e6e8732ec8ffe985dd6e3a
lukasz <lukrow@o2.pl>
2025-06-07 11:24:18 +0200

    newton updated

--- test/NewtonTests.jl
+++ test/NewtonTests.jl
@@ -3,30 +3,19 @@ using Test
 
 @testset "Newton tests" begin
     @test_nowarn begin
-        f(x) = x^2
+        f(x) = x^2-1
         dfdx(x) = 2x
         f2(x) = 5*x*x-2*x-7 # zeros at -1, 7/5
         df2dx(x) = 5x-2
-        x0 = 2.0
-        tol = 1e-10
-
-
-
-
-        x1, iter1 = newton(f, dfdx, x0)
+        # Test Newton's method with a simple quadratic function
+        x1, iter1 = newton(f, dfdx, 10.0)
         x2, iter2 = newton(f2, df2dx, -5.0)
-       
-        @test x1 ≈ 0.0 atol=1e-10
-  
-
-        @test x2 ≈ -1 atol=1e-10
-    
-
-
-        settings = SolverSettings(0.0, 0.0, 0.0, tol, 100)
-        @test_nowarn  newton(f, dfdx, x0, settings)
-
         
+        @test x1 ≈ 1.0 atol=1e-10
+        @test x2 ≈ -1.0 atol=1e-10
+    
+        settings = SolverSettings(0.0, 0.0, 0.0, 1e-6, 100)
+        @test_nowarn  newton(f, dfdx, 10.0, settings)
     end
 end
 
